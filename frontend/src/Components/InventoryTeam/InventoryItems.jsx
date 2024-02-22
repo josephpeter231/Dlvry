cc5951b1b93e6318028218165754c951586780bc
josephpeter231 <josephpeterjcit@gmail.com>
2024-02-22 08:28:18 +0530

    updated route for home

--- frontend/src/Components/InventoryTeam/InventoryItems.jsx
+++ frontend/src/Components/InventoryTeam/InventoryItems.jsx
@@ -20,7 +20,7 @@ const navigation = [
 const userNavigation = [
     { name: 'Your Profile', href: '#' },
     { name: 'Settings', href: '#' },
-    { name: 'Sign out', href: '#' },
+    { name: 'Sign out', href: '/' },
 ]
 
 function classNames(...classes) {
